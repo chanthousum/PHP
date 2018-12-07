3e512ad546a5518d9c260691e4b938c435ae564c
chanthousum <chanthousum2017@gmail.com>
2018-12-07 17:18:28 +0700

    remove

--- ajaxs/pages/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con= new MySQLi("localhost","root","","ajax");
-		$con->set_charset("utf8");
-		if($con->connect_error)
-		{
-			die("Error:".$con->connect_errno ."".$con->connect_error);
-		}
-?>
